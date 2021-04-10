08369ac7a94fa348869b2b0c08a4d6139b7429f3
Aidan Scannell <ascannel@umd.edu>
2021-04-10 02:09:47 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, request
+from PIL.Image import Image
+from logic.asset_manager import AssetManager
+import logic.jz_image_proc
+import logic.as_image_proc
+import logic.nst
+
+
+def create_app():
+    flask_app = Flask(__name__)
+
+    @flask_app.route("/logic/image_editor", methods=["GET"])
+    def get_apply_effect():
+
+        # Receive input
+        ui_input = request.get_json()
+
+        # Call pull helper method
+        input_img = pull_pillow_image(ui_input)
+
+        # Call alteration method specified by request
+        var = ui_input["effect"]
+        if var == "saturation":
+            altered_image = logic.jz_image_proc.change_saturation(input_img, ui_input["specifications"].toJson())
+        elif var == "hue":
+            altered_image = logic.as_image_proc.hue_editor(input_img, ui_input["specifications"].toJson())
+        elif var == "color-gradient":
+            altered_image = logic.as_image_proc.apply_gradient_editor(input_img, ui_input["specifications"].toJson())
+        elif var == "crop":
+            altered_image = logic.jz_image_proc.crop_editor(input_img, ui_input["specifications"].toJson())
+        elif var == "watermark":
+            altered_image = logic.jz_image_proc.add_watermark_image(input_img, ui_input["specifications"].toJson())
+        elif var == "blur":
+            altered_image = logic.jz_image_proc.gaussian_blur(input_img, ui_input["specifications"].toJson())
+        elif var == "opacity":
+            altered_image = logic.as_image_proc.opacity_editor(input_img, ui_input["specifications"].toJson())
+        elif var == "recoloration":
+            altered_image = logic.as_image_proc.apply_color_editor(input_img, ui_input["specifications"].toJson())
+        elif var == "rotate-image":
+            altered_image = logic.jz_image_proc.rotate_image(input_img, ui_input["specifications"].toJson())
+        elif var == "downscale-resolution":
+            altered_image = logic.jz_image_proc.scale_image(input_img, ui_input["specifications"].toJson())
+        elif var == "reverb":
+            altered_image = ImageEditor.reverb(input_img, ui_input["specifications"].toJson())
+        elif var == "eq":
+            altered_image = ImageEditor.eq(input_img, ui_input["specifications"].toJson())
+        elif var == "volume":
+            altered_image = ImageEditor.volume(input_img, ui_input["specifications"].toJson())
+        elif var == "rotate-video":
+            altered_image = logic.jz_image_proc.rotate_video(input_img, ui_input["specifications"].toJson())
+        else:
+            return None
+
+        # Call push helper method
+        url = push_pillow_image(altered_image, ui_input)
+
+        # Return url and image information to UI
+        return {"image_name": ui_input["image_name"], "url": url, "file_extension": ui_input["file_extension"]}
+
+    def pull_pillow_image(self, ui_input: dict):
+        # Receive input
+        image_name = ui_input["image_name"] + "." + ui_input["file_extension"]
+
+        # Receive Pil image from bucket
+        input_img = AssetManager.import_image_from_s3(image_name, ui_input["is_working_copy"])
+
+        return input_img
+
+    def push_pillow_image(self, altered_image: Image, ui_input: dict):
+        # Receive input
+        image_name = ui_input["image_name"] + "." + ui_input["file_extension"]
+
+        # Send new image back to S3 bucket and get url
+        url = AssetManager.upload_image_to_s3(altered_image, image_name, ui_input["is_working_copy"])
+
+        return url
---
+++ logic/as_image_proc.py
@@ -0,0 +1,137 @@
+from PIL import Image, ImageDraw
+import numpy as np
+import colorsys
+
+
+def hue_editor(input_img: Image, hue: int) -> Image:
+    """
+       Args:
+           input_img:  The image to be changed
+           hue:     The hue factor that will be set -
+
+       Returns:
+           output_img: Image with hue changed
+    """
+    #https://stackoverflow.com/questions/7274221/changing-image-hue-with-python-pil
+
+    if hue > 360 or hue < 0:
+        return None
+
+    img = input_img.convert('RGBA')
+    arr = np.array(np.asarray(img).astype('float'))
+
+    rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
+    hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
+
+    r, g, b, a = np.rollaxis(arr, axis=-1)
+    h, s, v = rgb_to_hsv(r, g, b)
+    h = (h + hue / 360.) % 1
+    r, g, b = hsv_to_rgb(h, s, v)
+    arr = np.dstack((r, g, b, a))
+
+    output_img = Image.fromarray(arr.astype('uint8'), 'RGBA')
+    return output_img
+
+
+def crop_editor(input_img: Image,
+                bounds: tuple = (int, int, int, int)) -> Image:
+    """
+       Args:
+           input_img:  The image to be changed
+           bounds:     tuple of 4 floats, being the left, top, right, bottom,
+                       of where the image is to be cropped by pixel count, that
+                       have to be within the image size
+
+       Returns:
+           output_img: Image that has been cropped
+    """
+
+    left, top, right, bottom = bounds
+    w, h = input_img.size
+    if left >= w or right >= w or top >= h or bottom >= h \
+            or right <= left or bottom <= top or left < 0 or top < 0:
+        return None
+    output_img = input_img.crop((left, top, right, bottom))
+    return output_img
+
+
+def opacity_editor(input_img: Image, alpha: int) -> Image:
+    """
+       Args:
+               input_img:  The image to be changed
+               alpha:      The value determining how opaque the image will be -
+                           0 being invisible, 100 being opaque
+
+       Returns:
+           output_img: Image with opacity changed
+    """
+
+    if alpha > 100 or alpha < 0:
+        return None
+
+    input_img.putalpha(int(alpha * 2.55))
+    return input_img
+
+
+def apply_color_editor(input_img: Image,
+                       color_mask: tuple = (255, 255, 255, 255)) -> Image:
+    """
+       Args:
+            input_img:  The image to be changed
+            color_mask: A tuple of four ints, being the rgba for the color
+                        mask
+
+       Returns:
+           output_img: Image with color mask applied changed
+    """
+
+    r, g, b, a = color_mask
+    if r < 0 or g < 0 or b < 0 or a < 0 or \
+            r > 255 or g > 255 or b > 255 or a > 255:
+        return None
+
+    color = Image.new('RGB', input_img.size, (r, g, b))
+    mask = Image.new('RGBA', input_img.size, (0, 0, 0, a))
+    output_image = Image.composite(input_img, color, mask).convert('RGB')
+    return output_image
+
+
+def apply_gradient_editor(input_img: Image, alpha: int,
+                          color_initial: tuple = (255, 255, 255),
+                          color_secondary: tuple = (255, 255, 255)) -> Image:
+    """
+       Args:
+           input_img:  The image to be changed
+           alpha: the alpha value of the mask to be applied
+           color_initial: A tuple of three ints, being the rgb for the first
+                          half of the color mask
+           color_secondary: A tuple of three ints, being the rgb for the
+                            second half of the color mask
+
+       Returns:
+           output_img: Image with color mask applied changed
+    """
+    #https://python-catalin.blogspot.com/2013/10/how-to-make-color-gradient-and-images.html
+
+    r, g, b = color_initial
+    r2, g2, b2 = color_secondary
+
+    if r < 0 or g < 0 or b < 0 or r > 255 or g > 255 or b > 255:
+        return None
+    if r2 < 0 or g2 < 0 or b2 < 0 or r2 > 255 or g2 > 255 or b2 > 255:
+        return None
+    if alpha < 0 or alpha > 255:
+        return None
+
+    color = Image.new("RGB", input_img.size, "#FFFFFF")
+    draw = ImageDraw.Draw(color)
+    dr = (r2 - r) / input_img.width
+    dg = (g2 - g) / input_img.width
+    db = (b2 - b) / input_img.width
+    for i in range(input_img.width):
+        r, g, b = r + dr, g + dg, b + db
+        draw.line((i, 0, i, input_img.height), fill=(int(r), int(g), int(b)))
+
+    mask = Image.new('RGBA', input_img.size, (0, 0, 0, alpha))
+    output_image = Image.composite(input_img, color, mask).convert('RGB')
+    return output_image
