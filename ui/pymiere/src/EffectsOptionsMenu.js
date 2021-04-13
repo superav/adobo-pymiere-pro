b46743abaa21ba94ede3fa5086ce20e20680a233
Arush Shah <arushah@umd.edu>
2021-04-13 11:22:12 -0400

    initial color menu with adjustables for hue, saturation, and gradient

--- ui/pymiere/src/EffectsOptionsMenu.js
+++ ui/pymiere/src/EffectsOptionsMenu.js
@@ -6,7 +6,6 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import TextOverlayTextInputFont from './TextOverlayTextInputFont'
-import TextOverlayColorPicker from './TextOverlayColorPicker';
 import TextOverlayLocationSelector from './TextOverlayLocationSelector';
 import Button from '@material-ui/core/Button'
 import ZoomEffect from './Effects/ZoomEffect'
@@ -14,6 +13,7 @@ import PixelViewer from './Effects/PixelViewer'
 import LightingOptionsMenu from './LightingOptionsMenu'
 import VignetteEffectPage from "./VignetteEffectPage"
 import SpecialEffectsOptions from './SpecialEffectsOptions'
+import ColorMenu from './ColorMenu'
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -82,6 +82,7 @@ export default function VerticalTabs(props) {
         <Tab label="Zoom" {...a11yProps(1)} />
         <Tab label="Pixel Viewer" {...a11yProps(2)} />
         <Tab label="Cropping" {...a11yProps(3)} />
+        <Tab label="Color Menu" {...a11yProps(3)} />
         <Tab label="Item Five" {...a11yProps(4)} />
         <Tab label="Special Effects" {...a11yProps(5)} />
         <Tab label="Lighting Options" {...a11yProps(6)} />
@@ -105,7 +106,10 @@ export default function VerticalTabs(props) {
         </ul>
       </TabPanel>
       <TabPanel value={value} index={3}>
-        Item Four
+        <ul>
+          <CropEffect getCanvas={props.getCanvas} setCanvas={props.setCanvas}/>
+          <ColorMenu />
+        </ul>
       </TabPanel>
       <TabPanel value={value} index={4}>
         <VignetteEffectPage getCanvas={props.getCanvas} setCanvas={props.setCanvas}/>
