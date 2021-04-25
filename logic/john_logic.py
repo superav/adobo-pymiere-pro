fb75f519803f0cd746bca68fb8dbdaff7af5b2bc
JZ0024 <jzhang60@umd.edu>
2021-04-25 17:06:02 -0400

    Fixed missing font bug and refactored code for readability

--- logic/john_logic.py
+++ logic/john_logic.py
@@ -22,18 +22,77 @@ def add_text_to_image(image=Image, specifications: list = [str, str, int, list, 
     offset = tuple(specifications[3])
     color = tuple(specifications[4])
 
-    valid_parameters = isinstance(image, Image.Image)
-    valid_parameters = valid_parameters and len(specifications) == 5
-    valid_parameters = valid_parameters and isinstance(text, str)
-    valid_parameters = valid_parameters and isinstance(font, str)
-    valid_parameters = valid_parameters and isinstance(size, int)
-    valid_parameters = valid_parameters and isinstance(offset, tuple)
-    valid_parameters = valid_parameters and isinstance(color, tuple)
-    if not valid_parameters:
+    if not isinstance(image, Image.Image):
         return None
+
+    if not __specifications_are_valid(specifications):
+        return None
+
     if len(offset) != 2 or len(color) != 3:
         return None
-    font = ImageFont.truetype("DejaVuSans.ttf", size)
+
+    try:
+        font = ImageFont.truetype(font, size)
+    except OSError:
+        print("Reverting to default font and font size")
+
+        font = ImageFont.load_default()
+
     draw = ImageDraw.Draw(image)
     draw.text(offset, text, color, font=font)
+
     return image
+
+# HELPER METHOD
+
+
+def __specifications_are_valid(specifications: list) -> bool:
+    if len(specifications) != 5:
+        return False
+
+    text = specifications[0]
+    font = specifications[1]
+    size = specifications[2]
+    offset = tuple(specifications[3])
+    color = tuple(specifications[4])
+
+    if not (isinstance(text, str) and isinstance(font, str)):
+        return False
+
+    if not (isinstance(size, int)):
+        return False
+
+    if not (isinstance(offset, tuple) and __offset_is_valid(offset)):
+        return False
+
+    if not (isinstance(color, tuple) and __colors_in_valid_range(color)):
+        return False
+
+    return True
+
+
+def __colors_in_valid_range(color: tuple) -> bool:
+    if len(color) != 3:
+        return False
+
+    red, green, blue = color
+
+    if not (isinstance(red, int) and isinstance(green, int) and isinstance(blue, int)):
+        return False
+
+    if not (0 <= red <= 255 and 0 <= green <= 255 and 0 <= blue <= 255):
+        return False
+
+    return True
+
+
+def __offset_is_valid(offset: tuple) -> bool:
+    if len(offset) != 2:
+        return False
+
+    x_position, y_position = offset
+
+    if not (isinstance(x_position, int) and isinstance(y_position, int)):
+        return False
+
+    return True
