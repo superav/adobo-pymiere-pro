3e683745369a517c632b2773732079e86856fe93
Arush Shah <arushah@umd.edu>
2021-04-28 22:14:40 -0400

    Added ui for redeye remover

--- ui/pymiere/src/SpecialEffectsOptions.js
+++ ui/pymiere/src/SpecialEffectsOptions.js
@@ -11,6 +11,26 @@ export function SpecialEffectsOptions (props) {
 
   const [effectChoice, setEffectChoice] = useState("None");
   const [solarizeValue, setSolarizeValue] = useState(0);
+  const [topLeftX, setTopLeftX] = useState(0);
+  const [topLeftY, setTopLeftY] = useState(0);
+  const [bottomRightX, setBottomRightX] = useState(0);
+  const [bottomRightY, setBottomRightY] = useState(0);
+
+  const handleTopLeftX = (e) => {
+    setTopLeftX(e.target.value);
+  }
+
+  const handleTopLeftY = (e) => {
+    setTopLeftY(e.target.value);
+  }
+
+  const handleBottomRightX = (e) => {
+    setBottomRightX(e.target.value);
+  }
+
+  const handleBottomRightY = (e) => {
+    setBottomRightY(e.target.value);
+  }
 
   const handleChangeEffectChoice = (event, newValue) => {
     setEffectChoice(newValue);
@@ -31,6 +51,9 @@ export function SpecialEffectsOptions (props) {
     else if (effectChoice === "solarize") {
       props.applyFilter("solarize", parseInt(solarizeValue));
     }
+    else if (effectChoice == "red-eye-remover") {
+      props.applyFilter("red-eye-remover", [parseInt(topLeftX), parseInt(topLeftY), parseInt(bottomRightX), parseInt(bottomRightY)]);
+    }
     else if (effectChoice == "autocontrast") {
       props.applyFilter("autocontrast", [0.5]);
     }
@@ -43,7 +66,16 @@ export function SpecialEffectsOptions (props) {
         <RadioGroup aria-label="Special Effects" name="Special Effects" value={effectChoice} onChange={handleChangeEffectChoice}>
           <FormControlLabel value="mosaic" control={<Radio />} label="Mosaic Filter" />
           <FormControlLabel value="solarize" control={<Radio />} label="Solarize" />
-          <TextField id="outlined-basic" label="Input" variant="outlined" value={solarizeValue} onChange={handleSolarizeChange}/>
+          <TextField id="outlined-basic" label="Solarize Threshold (0-255)" variant="outlined" value={solarizeValue} onChange={handleSolarizeChange}/>
+          <FormControlLabel value="red-eye-remover" control={<Radio />} label="Redeye Removal" />
+          <TextField id="outlined-basic" label="Top Left X Coordinate" variant="outlined" size="small" value={topLeftX} onChange={handleTopLeftX}/>
+          <br></br>
+          <TextField id="outlined-basic" label="Top Left Y Coordinate" variant="outlined" size="small" value={topLeftY} onChange={handleTopLeftY}/>
+          <br></br>
+          <TextField id="outlined-basic" label="Bottom Right X Coordinate" variant="outlined" size="small" value={bottomRightX} onChange={handleBottomRightX}/>
+          <br></br>
+          <TextField id="outlined-basic" label="Bottom Right Y Coordinate" variant="outlined" size="small" value={bottomRightY} onChange={handleBottomRightY}/>
+          <br></br>
           <FormControlLabel value="autocontrast" disabled="true" control={<Radio /> } label="Autocontrast" />
         </RadioGroup>
       </FormControl>
