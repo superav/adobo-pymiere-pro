1efdaac716e0865ebaa5d767319597df23bce3dd
Arush Shah <arushah@umd.edu>
2021-04-29 13:40:31 -0400

    added ui for frame selection

---
+++ ui/pymiere/src/FrameMenu.js
@@ -0,0 +1,67 @@
+import React, { Component, useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
+
+export default function FrameMenu(props) {
+  
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  function handleRChange(e, v) {
+    setRed(v);
+  }
+  
+  function handleGChange (e, v) {
+    setGreen(v);
+  }
+  
+  function handleBChange(e, v) {
+    setBlue(v);
+  }
+
+  function confirmFrame() {
+    props.applyFilter("frame", [parseInt(red), parseInt(green), parseInt(blue)])
+  }
+
+  return (
+    <div>
+      <Typography id="color" gutterBottom>
+      <b>Frame Color</b>
+      </Typography>
+
+      <h4>Red</h4>
+      <Slider
+        value={red}
+        onChange={handleRChange}
+        aria-labelledby="input-slider"
+        valueLabelDisplay="auto"
+        min={0}
+        max={255}
+      />
+      
+      <h4>Green</h4>
+      <Slider
+        value={green}
+        onChange={handleGChange}
+        aria-labelledby="input-slider"
+        valueLabelDisplay="auto"
+        min={0}
+        max={255}
+      />
+
+      <h4>Blue</h4>
+      <Slider
+        value={blue}
+        onChange={handleBChange}
+        aria-labelledby="input-slider"
+        valueLabelDisplay="auto"
+        min={0}
+        max={255}
+      />
+      <br></br>
+      <Button variant="contained" color="primary" onClick={confirmFrame}>Add Frame</Button>
+    </div>
+  )
+}
