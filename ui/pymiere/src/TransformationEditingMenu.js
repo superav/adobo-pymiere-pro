e18dc0f830fdde529f7c7e725d060301c37dcae4
Phong Nguyen <nghe191@gmail.com>
2021-05-08 20:36:08 -0400

    Added original features in transformation menu back and replace Watermark with a new one

--- ui/pymiere/src/TransformationEditingMenu.js
+++ ui/pymiere/src/TransformationEditingMenu.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Slider from "@material-ui/core/Slider";
 import { withSnackbar } from 'notistack';
 
 class TransformationEditingMenu extends Component {
   constructor(props) {
     super(props);
     this.emojiPreview = React.createRef();
+    this.state = {
+      rValue: "",
+      gValue: "",
+      bValue: "",
+      aValue: "",
+      backgroundName: "",
+      opacityValue: 1,
+      blurValue: 1,
+    };
   }
 
   componentDidMount () {
@@ -60,13 +71,138 @@ class TransformationEditingMenu extends Component {
     this.updateEmojiBackend();
   }
 
+  handleChange = (name) => (event) => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  applyRecolorationFilter = () => {
+    console.log("r value: " + this.state.rValue);
+    console.log("g value: " + this.state.gValue);
+    console.log("b value: " + this.state.bValue);
+    this.props.applyFilter("recoloration", [
+      parseInt(this.state.rValue),
+      parseInt(this.state.gValue),
+      parseInt(this.state.bValue),
+      parseInt(this.state.aValue),
+    ]);
+  };
+
+  changeBackground = () => {
+    console.log("background name: " + this.state.backgroundName);
+  };
+
+  changeOpacity = (e, val) => {
+    this.setState({
+      opacityValue: val,
+    });
+  };
+
+  confirmOpacityChange = () => {
+    console.log("Opacity: " + this.state.opacityValue);
+    this.props.applyFilter("opacity", parseInt(this.state.opacityValue));
+  }
+
+  confirmBlurChange = () => {
+    console.log("Opacity: " + this.state.blurValue);
+    this.props.applyFilter("blur", parseInt(this.state.blurValue));
+  }
+
+  applyGaussianBlur = (e, val) => {
+    this.setState({
+      blurValue: val,
+    });
+  };
+
   render() {
     return <div>
-      <h3>Watermark</h3>
-      <canvas ref={this.emojiPreview} width={150} height={150}/><br/>
-      <br/><br/>
-      <Button variant="contained" color="primary" onClick={this.onApply}>Apply Watermark</Button>
-    </div>
+        <h4>Change Opacity:</h4>
+        <Slider
+          value={this.state.opacityValue}
+          onChange={this.changeOpacity}
+          aria-labelledby="discrete-slider-small-steps"
+          min={0}
+          max={100}
+          step={1}
+          valueLabelDisplay="auto"
+        ></Slider>
+        <Button variant="contained" color="primary" onClick={this.confirmOpacityChange}>Change Opactiy</Button>
+
+        <h4>Gaussian Blur:</h4>
+        <Slider
+          value={this.state.blurValue}
+          onChange={this.applyGaussianBlur}
+          aria-labelledby="discrete-slider-small-steps"
+          min={0}
+          max={9}
+          step={1}
+          valueLabelDisplay="auto"
+        ></Slider>
+        <Button variant="contained" color="primary" onClick={this.confirmBlurChange}>Change Blur</Button>
+
+        <h4>Change Background:</h4>
+        <TextField
+          id="outlined-basic"
+          value={this.state.backgroundName}
+          onChange={this.handleChange("backgroundName")}
+          label="Background Name"
+          variant="outlined"
+        />
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.changeBackground}
+          disabled="true"
+        >
+          Select Background
+        </Button>
+        <br />
+        <h4>Watermark</h4>
+        <canvas ref={this.emojiPreview} width={150} height={150}/><br/>
+        <Button variant="contained" color="primary" onClick={this.onApply}>Apply Watermark</Button>
+        <h4>Apply Recoloration Filter:</h4>
+        <TextField
+          id="outlined-basic"
+          value={this.state.rValue}
+          onChange={this.handleChange("rValue")}
+          label="R Value (0 - 255)"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="outlined-basic"
+          value={this.state.gValue}
+          onChange={this.handleChange("gValue")}
+          label="G Value (0 - 255)"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="outlined-basic"
+          value={this.state.bValue}
+          onChange={this.handleChange("bValue")}
+          label="B Value (0 - 255)"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="outlined-basic"
+          value={this.state.aValue}
+          onChange={this.handleChange("aValue")}
+          label="A Value (0 - 255)"
+          variant="outlined"
+        />
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.applyRecolorationFilter}
+        >
+          Apply Recoloration
+        </Button>
+      </div>
   }
 }
 
