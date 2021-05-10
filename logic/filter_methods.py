542da96edbaf10ecb54326c7afb242dfebb35a3a
JZ0024 <jzhang60@umd.edu>
2021-05-10 14:36:46 -0400

    fixed more pipeline issues

--- logic/filter_methods.py
+++ logic/filter_methods.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageFilter, ImageOps
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 from flask import abort
 
 """
@@ -146,44 +146,44 @@ def apply_red_eye_filter(input_img: Image, specifications: list) -> Image:
     return output_img
 
 
-def apply_vignette(input_img: Image) -> Image:
-    """
-        Args:
-            input_img:  The image to be changed
-
-        Returns:
-            PIL.Image: Image with vignette applied
-    """
-    # https://www.geeksforgeeks.org/create-a-vignette-filter-using-python-opencv/
-    if not isinstance(input_img, Image.Image):
-        abort(500, description="apply_contrast: invalid input types")
-
-    # helper to convert to cv
-    input_img = __convert_to_rgb(input_img)
-    input_cv = __pil_to_cv(input_img)
-
-    # Extracting the height and width of an image
-    rows, cols = input_cv.shape[:2]
-
-    # generating vignette mask using Gaussian resultant_kernels
-    x_resultant_kernel = cv2.getGaussianKernel(cols, int(cols/2.4))
-    y_resultant_kernel = cv2.getGaussianKernel(rows, int(rows/2.4))
-
-    # generating resultant_kernel matrix
-    resultant_kernel = y_resultant_kernel * x_resultant_kernel.T
-
-    # creating mask and normalising by using np.linalg function
-    mask = 255 * resultant_kernel / np.linalg.norm(resultant_kernel)
-    output = np.copy(input_cv)
-
-    # applying the mask to each channel in the input image
-    for i in range(3):
-        output[:, :, i] = output[:, :, i] * mask
-
-    # helper to convert to pil
-    output_img = __cv_to_pil(output)
-
-    return output_img
+# def apply_vignette(input_img: Image) -> Image:
+#     """
+#         Args:
+#             input_img:  The image to be changed
+#
+#         Returns:
+#             PIL.Image: Image with vignette applied
+#     """
+#     # https://www.geeksforgeeks.org/create-a-vignette-filter-using-python-opencv/
+#     if not isinstance(input_img, Image.Image):
+#         abort(500, description="apply_contrast: invalid input types")
+#
+#     # helper to convert to cv
+#     input_img = __convert_to_rgb(input_img)
+#     input_cv = __pil_to_cv(input_img)
+#
+#     # Extracting the height and width of an image
+#     rows, cols = input_cv.shape[:2]
+#
+#     # generating vignette mask using Gaussian resultant_kernels
+#     x_resultant_kernel = cv2.getGaussianKernel(cols, int(cols/2.4))
+#     y_resultant_kernel = cv2.getGaussianKernel(rows, int(rows/2.4))
+#
+#     # generating resultant_kernel matrix
+#     resultant_kernel = y_resultant_kernel * x_resultant_kernel.T
+#
+#     # creating mask and normalising by using np.linalg function
+#     mask = 255 * resultant_kernel / np.linalg.norm(resultant_kernel)
+#     output = np.copy(input_cv)
+#
+#     # applying the mask to each channel in the input image
+#     for i in range(3):
+#         output[:, :, i] = output[:, :, i] * mask
+#
+#     # helper to convert to pil
+#     output_img = __cv_to_pil(output)
+#
+#     return output_img
 
 # HELPER METHODS
 
@@ -203,31 +203,31 @@ def __convert_to_rgb(image: Image):
     return background
 
 
-def __cv_to_pil(input_img):
-    """
-        Args:
-            input_img:  CV2 image to be converted to PIL
+# def __cv_to_pil(input_img):
+#     """
+#         Args:
+#             input_img:  CV2 image to be converted to PIL
+#
+#         Returns:
+#             output_img: new PIL image
+#     """
+#     # https://stackoverflow.com/questions/43232813/convert-opencv-image-format-to-pil-image-format
+#     cv_image = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
+#     output_img = Image.fromarray(cv_image)
+#
+#     return output_img
 
-        Returns:
-            output_img: new PIL image
-    """
-    # https://stackoverflow.com/questions/43232813/convert-opencv-image-format-to-pil-image-format
-    cv_image = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
-    output_img = Image.fromarray(cv_image)
 
-    return output_img
-
-
-def __pil_to_cv(input_img):
-    """
-        Args:
-            input_img:  Pillow image to be converted to CV2
-
-        Returns:
-            output_img: new CV2 image
-    """
-    # https://stackoverflow.com/questions/14134892/convert-image-from-pil-to-opencv-format
-    open_cv_image = np.array(input_img)
-    output_img = open_cv_image[:, :, ::-1].copy()
-
-    return output_img
+# def __pil_to_cv(input_img):
+#     """
+#         Args:
+#             input_img:  Pillow image to be converted to CV2
+#
+#         Returns:
+#             output_img: new CV2 image
+#     """
+#     # https://stackoverflow.com/questions/14134892/convert-image-from-pil-to-opencv-format
+#     open_cv_image = np.array(input_img)
+#     output_img = open_cv_image[:, :, ::-1].copy()
+#
+#     return output_img
